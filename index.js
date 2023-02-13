83a1d0bbc873969159e660f8f6128a37295eae74
GalaxyShard <swift.galaxyshard@gmail.com>
2023-02-13 10:37:53 -0500

    added highlighting

--- index.js
+++ index.js
@@ -3,21 +3,29 @@ const eventsWeekContent = document.getElementById("eventsWeekContent");
 const comingUpContent = document.getElementById("comingUpContent");
 const pastEventsContent = document.getElementById("pastEventsContent");
 
-function setActive(content) {
+const eventsWeekTab = document.getElementById("eventsWeekTab");
+const comingUpTab = document.getElementById("comingUpTab");
+const pastEventsTab = document.getElementById("pastEventsTab");
+
+function setActive(tab, content) {
     eventsWeekContent.style.display = "none";
     comingUpContent.style.display = "none";
     pastEventsContent.style.display = "none";
+    eventsWeekTab.classList.remove("highlight");
+    comingUpTab.classList.remove("highlight");
+    pastEventsTab.classList.remove("highlight");
 
     content.style.display = "block";
+    tab.classList.add("highlight");
 }
-setActive(eventsWeekContent);
+setActive(eventsWeekTab, eventsWeekContent);
 
 function selectEventsWeek() {
-    setActive(eventsWeekContent);
+    setActive(eventsWeekTab, eventsWeekContent);
 }
 function selectComingUp() {
-    setActive(comingUpContent);
+    setActive(comingUpTab, comingUpContent);
 }
 function selectPastEvents() {
-    setActive(pastEventsContent);
+    setActive(pastEventsTab, pastEventsContent);
 }
